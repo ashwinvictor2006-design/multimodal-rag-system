cd5f2efe25a2a7c0fbbf66de1afd440295eb5ce6
ashwinvictor2006-design <ashwinvictor2006@gmail.com>
2026-06-23 10:33:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from app.api.upload import router as upload_router
+from app.api.query import router as query_router
 
-app = FastAPI(title="Multimodal RAG System")
+app = FastAPI(
+    title="Multimodal RAG System",
+    version="1.0.0"
+)
+
+app.include_router(upload_router)
+app.include_router(query_router)
 
 @app.get("/")
 def home():
-    return {"message": "Multimodal RAG System Running"}
+    return {
+        "message": "Multimodal RAG System Running"
+    }
